6316ef0207f3a8726484dfe7ca5ac2091fe72530
wonggwan <wangjun200805@163.com>
2017-10-31 12:35:51 +0800

    update

---
+++ Backup/lab5_backup/app/src/main/java/io/github/wonggwan/lab5/ListViewAdapter.java
@@ -0,0 +1,124 @@
+package io.github.wonggwan.lab5;
+
+/**
+ * Created by wonggwan on 2017/10/28.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wonggwan on 2017/10/22.
+ */
+
+public class ListViewAdapter extends BaseAdapter{
+    private Context my;
+    private List<Products> list = new ArrayList<>();
+
+    public ListViewAdapter(Context context) {
+        list = new ArrayList<Products>();
+        my = context;
+    }
+
+    //initilize the listener
+    interface OnItemClickLitener {
+        void onItemClick(View view, int position);
+        void onItemLongClick(View view, int position);
+    }
+    private OnItemClickLitener mOnItemClickLitener;
+    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
+        this.mOnItemClickLitener = mOnItemClickLitener;
+    }
+
+    //set up the required viewholder
+    private static class ViewHolder {
+        TextView getnumber;
+        TextView myproductinfo;
+        TextView myfirstletter;
+        TextView myprice;
+    }
+    public void removeData(int position) {
+        list.remove(position);
+        this.notifyDataSetChanged();
+    }
+    public void addData(Products product) {
+        list.add(product);
+        this.notifyDataSetChanged();
+    }
+    public void clearData() {
+        list.clear();
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return list==null?0:list.size();
+    }
+    //count the number of the items in the list
+
+    @Override
+    public Products getItem(int position) {
+        return list.get(position);
+    }
+    //send back the position of the product that was chosen
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    //send back the position information of the chosen product
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(my);
+        ViewHolder holder = null;  //initialize viewholder
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.listviewitem, null);
+            /*
+            * Viewholder could hold the information you want to put in a certain area
+            * in listview, we have the first letter; complete name and the price as the required information
+            */
+            holder = new ViewHolder();
+            holder.getnumber = (TextView) convertView.findViewById(R.id.id_ItemNum);
+            holder.myfirstletter = (TextView) convertView.findViewById(R.id.id_Firstletter);
+            holder.myproductinfo = (TextView) convertView.findViewById(R.id.id_completename);
+            holder.myprice = (TextView) convertView.findViewById(R.id.id_ItemPrice);
+            convertView.setTag(holder);
+        }
+        else { holder = (ViewHolder) convertView.getTag(); }
+
+        if (mOnItemClickLitener != null) {
+            final View finalConvertView = convertView;
+            //short click listener
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOnItemClickLitener.onItemClick(finalConvertView,position);
+                }
+            });
+
+            //long click listener
+            convertView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    mOnItemClickLitener.onItemLongClick(finalConvertView,position);
+                    return false;
+                }
+            });
+        }
+
+        //put the corresponding information to the correct widget
+        holder.getnumber.setText("购买数量："+Integer.toString(list.get(position).how_many_in_the_cart()));
+        holder.myprice.setText( list.get(position).getPrice() );
+        holder.myproductinfo.setText( list.get(position).getName() );
+        holder.myfirstletter.setText(list.get(position).getFirst_letter());
+        return convertView;
+    }
+
+}
---
+++ Backup/lab5_backup/app/src/main/java/io/github/wonggwan/lab5/Products.java
@@ -0,0 +1,79 @@
+package io.github.wonggwan.lab5;
+
+/**
+ * Created by wonggwan on 2017/10/28.
+ */
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Products implements Parcelable{
+    private Boolean isstar;
+    private Integer numberincart;
+    private String  name;
+    private String  price;
+    private Integer image;
+    private String  productinfo;
+    private String  First_letter;
+
+    Products(String itemname, String fletter, String Price, String info, Integer img) {
+        name = itemname;
+        First_letter = fletter;
+        price = Price;
+        productinfo = info;
+        image = img;
+        numberincart = 0;
+        isstar = Boolean.FALSE;
+    }
+
+    //send basic information to the adapters
+    public String getName() { return name; }
+    public String getFirst_letter(){ return First_letter;}
+    public String getPrice() { return price; }
+    public Integer getImgObject() { return image; }
+    public String getSpecialInfo() { return productinfo; }
+
+
+    public Boolean get_isstar() { return isstar; }
+    public Boolean get_iscart() { return numberincart>0; }
+    public Integer how_many_in_the_cart() { return numberincart;}
+
+    //these two functions are for the mainactivity to update the condition in the detail screen
+    public void set_cart(Integer addtocart) { this.numberincart = addtocart; }
+    public void set_star(Boolean star) { this.isstar = star; }
+
+    protected Products(Parcel in){
+        name = in.readString();
+        price = in.readString();
+        productinfo = in.readString();
+        image = in.readInt();
+        isstar = in.readByte() != 0;
+        numberincart = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(price);
+        dest.writeString(productinfo);
+        dest.writeInt(image);
+        dest.writeInt(isstar ? 1 : 0);
+        dest.writeInt(numberincart);
+    }
+
+    public static final Parcelable.Creator<Products> CREATOR
+            = new Parcelable.Creator<Products>(){
+        @Override
+        public Products createFromParcel(Parcel source) {
+            return new Products(source);
+        }
+        @Override
+        public Products[] newArray(int size) {
+            return new Products[size];
+        }
+    };
+}
