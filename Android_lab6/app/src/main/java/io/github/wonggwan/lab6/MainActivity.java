b56f294b5bcf510df2dcde2c521f62f81c9856ce
David Wong <davidwong@wonggwandeMBP.lan>
2017-11-15 13:03:50 +0800

    update

---
+++ Android_lab6/app/src/main/java/io/github/wonggwan/lab6/MainActivity.java
@@ -0,0 +1,262 @@
+package io.github.wonggwan.lab6;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.app.Notification;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.os.health.PackageHealthStats;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView Condition, cur, dur;
+    private SeekBar seekBar;
+
+    private Button PlayPausebutton, Stopbutton, Quitbutton;
+
+
+    private ObjectAnimator animator;
+
+    private IBinder mBinder;
+    private Handler mHandler;
+
+    private SimpleDateFormat time = new SimpleDateFormat("mm:ss");
+
+    private void bindServiceConnection() {
+        Intent intent = new Intent(MainActivity.this, MusicService.class);
+        startService(intent);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBinder = service;
+            Log.i("Service is ", "connected");
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceConnection = null;
+        }
+    };
+
+
+
+
+    private void init() {
+        PlayPausebutton = (Button) findViewById(R.id.playpause);
+        Stopbutton = (Button) findViewById(R.id.stop);
+        Quitbutton = (Button) findViewById(R.id.quit);
+        Condition = (TextView) findViewById(R.id.condition);
+
+        cur = (TextView) findViewById(R.id.currentTime);
+        dur = (TextView) findViewById(R.id.totaltime);
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+
+
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.Image);
+        animator = ObjectAnimator.ofFloat(imageView, "rotation", 0, 360);
+        animator.setDuration(24000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(-1);
+        animator.setRepeatMode(ValueAnimator.RESTART);
+    }
+
+
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        bindServiceConnection();
+        PlayPausebutton.setText("PLAY");
+        Condition.setText("Stopped");
+
+
+
+
+
+        final Thread mThread = new Thread()
+        {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (serviceConnection != null) {
+                        mHandler.obtainMessage(123).sendToTarget();
+                    }
+                }
+            }
+        };
+
+        mThread.start();
+        mHandler = new Handler(){
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                switch (msg.what){
+                    case 123:
+                        try {
+                            int code = 104;
+                            Parcel data = Parcel.obtain();
+                            Parcel reply = Parcel.obtain();
+                            mBinder.transact(code,data,reply,0);
+
+                            Bundle bundle = reply.readBundle();
+
+                            seekBar.setProgress(bundle.getInt("prog"));
+                            seekBar.setMax(bundle.getInt("dur"));
+                            cur.setText(time.format(bundle.getInt("cur")));
+                            dur.setText(time.format(bundle.getInt("dur")));
+
+
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+            }
+        };
+
+
+        setSeekBar();
+        setbnt();
+    }
+
+    private void setSeekBar() {
+        seekBar.setProgress(0);
+        seekBar.setMax(100);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBar.setProgress(progress);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                try{
+                    int progress = seekBar.getProgress();
+                    int code = 105;
+                    Parcel data = Parcel.obtain();
+                    data.writeInt(progress);
+                    Parcel reply = Parcel.obtain();
+                    mBinder.transact(code,data,reply,0);
+                    return;
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
+    private void setbnt() {
+        PlayPausebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = MainActivity.this.PlayPausebutton.getText().toString();
+                try{
+                    if(text.equals("PLAY")) {
+
+                        PlayPausebutton.setText("PAUSE");
+                        Condition.setText("Playing");
+                        if (animator.isPaused()) {
+                            animator.resume();
+                        }
+                        else {
+                            animator.start();
+                        }
+
+                    }
+                    else if (text.equals("PAUSE")) {
+                        PlayPausebutton.setText("PLAY");
+                        Condition.setText("Paused");
+                        animator.pause();
+                    }
+                    int code = 101;
+                    Parcel data = Parcel.obtain();
+                    Parcel reply = Parcel.obtain();
+                    mBinder.transact(code,data,reply,0);
+                }
+                catch (RemoteException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Stopbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlayPausebutton.setText("PLAY");
+                Condition.setText("Stopped");
+                try{
+                    int code = 102;
+                    Parcel data = Parcel.obtain();
+                    Parcel reply = Parcel.obtain();
+                    mBinder.transact(code,data,reply,0);
+
+                    
+                    Bundle bundle = reply.readBundle();
+                    cur.setText(time.format(bundle.getInt("cur")));
+                    dur.setText(time.format(bundle.getInt("dur")));
+                    seekBar.setProgress(bundle.getInt("prog"));
+                    animator.end();
+                    return;
+                }
+                catch (RemoteException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Quitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unbindService(serviceConnection);
+                serviceConnection = null;
+                try {
+                    MainActivity.this.finish();
+                    System.exit(0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
---
+++ Android_lab6/app/src/main/java/io/github/wonggwan/lab6/MusicService.java
@@ -0,0 +1,140 @@
+package io.github.wonggwan.lab6;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.widget.Toast;
+
+
+import java.io.IOException;
+
+/**
+ * Created by wonggwan on 2017/11/10.
+ */
+
+public class MusicService extends Service {
+    public MediaPlayer player = null;
+
+    public IBinder mBinder = new MyBinder();
+
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public int isStopped = 0;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+    public class MyBinder extends Binder {
+        protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            switch (code) {
+                case 101:
+                    playOrPause();
+                    break;
+                case 102:
+                    stop();
+                    break;
+                case 103:
+                    onDestroy();
+                    break;
+                case 104:
+                    if(player != null) {
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("cur", getposorprog());
+                        bundle.putInt("dur", getdur());
+                        bundle.putInt("prog", getposorprog());
+                        reply.writeBundle(bundle);
+                    }
+                    break;
+                case 105:
+                    int i = data.readInt();
+                    player.seekTo(i);
+                    System.out.println(i);
+                    break;
+            }
+            return super.onTransact(code, data, reply, flags);
+        }
+    }
+
+
+    public void playOrPause() {
+        if(player == null) {
+            player = new MediaPlayer();
+            Toast.makeText(MusicService.this, "正在播放：늑는다", Toast.LENGTH_SHORT).show();
+            try {
+                player.setDataSource("/data/melt.mp3");
+                player.prepare();
+                player.seekTo(0);
+                player.setLooping(true);
+                player.start();
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if(player != null) {
+            if (player.isPlaying()) {
+                player.pause();
+            }
+            else {
+                player.start();
+            }
+        }
+    }
+
+    public void stop() {
+        if (player != null) {
+            player.stop();
+            isStopped = 1;
+        }
+        try {
+            if(isStopped == 1) {
+                player.reset();
+                player.setDataSource("/data/melt.mp3");
+                player.prepare();
+                player.seekTo(0);
+                player.setLooping(true);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getdur() {
+        if (player != null) {
+            return player.getDuration();
+        }
+        else return 0;
+    }
+
+    public int getposorprog() {
+        return player.getCurrentPosition();
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(player != null) {
+            player.stop();
+            player.reset();
+            player.release();
+            player = null;
+        }
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+}
